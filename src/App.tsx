04f9f426102a32f4329f41effd6e0d5991f7f00d
Sukanta Paul <paul.sukanta@gmail.com>
2024-03-25 13:50:28 +0530

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="grid place-items-center h-screen">
+      <div className="w-96 font-bold">Hello World</div>
+    </div>
+  );
+}
+
+export default App;
